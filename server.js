0679ebeff7b72fa4f4f9afb9d21f4f7619a33949
fincoder <aaronhardy6@gmail.com>
2023-04-17 16:25:04 +0900

    added source

--- server.js
+++ server.js
@@ -45,6 +45,23 @@ app.get(`/foxnews`, async function (req, res) {
   }
 });
 
+// @ GET /dailysignal
+app.get(`/dailysignal`, async function (req, res) {
+  const options = {
+    method: "GET",
+  };
+
+  const url = "https://dailysignal.com/feed/";
+  try {
+    let response = await fetch(url, options);
+    response = await response.text();
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: `Internal Server Error.` });
+  }
+});
+
 // Set up Express Listener
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
